8f1ed0ebcbe7112208d502ed268d9cf75e66c7b1
Jorge Mina <minajorge@hotmail.com>
2020-11-23 09:49:08 -0500

    Terminado Informe Pdf

--- src/main/java/com/cdm/gastos/security/WebSecurityConfig.java
+++ src/main/java/com/cdm/gastos/security/WebSecurityConfig.java
@@ -57,8 +57,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/informe/pdf/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
